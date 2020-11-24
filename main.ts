98c28cf7fe82fcba6db711255aafa0d358ce71da
ivy0116 <71689324+ivy0116@users.noreply.github.com>
2020-11-24 15:57:25 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     A.set(LedSpriteProperty.Direction, 180)
+    A.move(1)
 })
 input.onButtonPressed(Button.B, function () {
     B.set(LedSpriteProperty.X, 180)
+    B.move(1)
 })
 let B: game.LedSprite = null
 let A: game.LedSprite = null
